10b8abae34a5894c4a9609cd177de999873dc5ba
konstantin-petruschin <konstantin-petruschin1988mail.ru>
2023-11-07 19:21:19 +0300

    Разметка и стилизация слайда

--- gulpfile.js
+++ gulpfile.js
@@ -134,7 +134,7 @@ export const watcher = () => {
   gulp.watch('source/sass/**/*.scss', gulp.series(styles));
   gulp.watch('source/js/main.js', gulp.series(scripts, reload));
   gulp.watch('source/*.html').on('change', gulp.series(html, reload));
-  gulp.watch('source/img/**/*.{jpg,png,svg}', gulp.series(copyImages, createWebp, styles, html, reload));
+  gulp.watch('source/img/**/*.{jpg,png,svg}', gulp.series(copyImages, createWebp, svgTask, styles, html, reload));
   gulp.watch('source/img/sprite/*.svg', gulp.series(sprite, styles, html, reload));
 }
 
