b3711179474873970ccc3f6b4a455c7648d34f30
konstantin-petruschin <konstantin-petruschin1988mail.ru>
2023-11-13 15:58:54 +0300

    Подключение карты

--- source/js/main.js
+++ source/js/main.js
@@ -10,3 +10,33 @@ toogleButtonElement.addEventListener('click', function () {
   toogleButtonElement.classList.toggle('main-header__toggle--closed');
   menuElement.classList.toggle('main-nav--opened');
 })
+
+
+const mapWrapper = document.querySelector('#map');
+
+if (mapWrapper) {
+  ymaps.ready(init);
+}
+
+function init() {
+  const myMap = new ymaps.Map('map', {
+    center: [59.938631, 30.323037],
+    zoom: 17
+  });
+
+  let imageSource = {
+    src: '../img/icons/map-pin.svg',
+    size: [67, 100],
+    offset: [-40, -100]
+  }
+
+  myMap.geoObjects.add(new ymaps.Placemark([59.938631, 30.323037],
+    {
+      hintContent: '<div class="map__hint">Магазин «Мишка»</div>'
+    }, {
+      iconLayout: 'default#image',
+      iconImageHref: imageSource.src,
+      iconImageSize: imageSource.size,
+      iconImageOffset: imageSource.offset
+    }))
+}
