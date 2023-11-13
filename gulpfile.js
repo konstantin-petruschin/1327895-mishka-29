cb07119681821b03b9b4cde2f6eced4aa0a5083c
konstantin-petruschin <konstantin-petruschin1988mail.ru>
2023-11-14 00:28:35 +0300

    Правки

--- gulpfile.js
+++ gulpfile.js
@@ -91,8 +91,8 @@ const sprite = () => {
 const copy = (done) => {
   gulp.src([
     'source/fonts/*.{woff,woff2}',
-    // 'source/*.ico',
-    // 'source/manifest.webmanifest'
+    'source/*.ico',
+    'source/manifest.webmanifest'
   ], {
     base: 'source'
   })
